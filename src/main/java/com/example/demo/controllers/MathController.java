3070e665b3ff1daec7cb4785345f9cd6213fce1d
gerry.bradley <gerry bradley>
2017-08-10 20:46:43 -0700

    Math Controller added

--- src/main/java/com/example/demo/controllers/MathController.java
+++ src/main/java/com/example/demo/controllers/MathController.java
@@ -15,14 +15,16 @@ public class MathController {
 	// http://localhost:8080/math/adder POST
 
 	@PostMapping("adder")
-	public String addTwoNumbers(@RequestParam(name="left") int first, @RequestParam(name="right") double second, Model model) {
+	public String addTwoNumbers(@RequestParam(name = "left") int first, @RequestParam(name = "right") double second,
+			Model model) {
 		Adder adder = new Adder(first, second);
 		double result = adder.calculate();
-		
 
-		
 		model.addAttribute("sum", result);
 		return "helloworld/sum-result";
 	}
+	
+
+
 
 }
